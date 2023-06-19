1a952e4af2053c4a6294bd25bb67388e6c327554
Danil Efremov <danileph@bk.ru>
2023-06-19 18:16:04 +0300

    Added a line with checking if window exists.

--- src/hooks/useWindowDimensions.ts
+++ src/hooks/useWindowDimensions.ts
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 
 function getWindowDimensions() {
+  if (!window) return {width: 0, height: 0};
+
   const { innerWidth: width, innerHeight: height } = window;
   return {
     width,
@@ -9,12 +11,13 @@ function getWindowDimensions() {
 }
 
 function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const [windowDimensions, setWindowDimensions] = useState({width: 0, height: 0});
 
   useEffect(() => {
     function handleResize() {
       setWindowDimensions(getWindowDimensions());
     }
+    handleResize();
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
