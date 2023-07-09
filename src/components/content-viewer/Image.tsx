37c2170fe2b8ad40227c20cac8d557fc903cbd36
Danil Efremov <danileph@bk.ru>
2023-07-09 21:57:27 +0300

    Some fixes

--- src/components/content-viewer/Image.tsx
+++ src/components/content-viewer/Image.tsx
@@ -3,7 +3,7 @@ import {default as ImageNext, ImageLoader} from 'next/image';
 import Skeleton from "@/components/skeleton/Skeleton";
 
 interface IImageProps extends Omit<React.HTMLAttributes<HTMLElement>, 'children'> {
-  src: string;
+  src: string | undefined;
   alt: string;
   loading?: "eager" | "lazy" | undefined;
   skeletonClassName?: string;
@@ -24,7 +24,7 @@ const Image: FC<IImageProps> = ({skeletonClassName, src, alt, className, ...othe
 
   return (
     <div className={`relative ${className}`}>
-      <img onLoad={() => setIsLoaded(true)} className={`${isLoaded ? 'visible' : 'invisible absolute'}`} src={src} alt={alt} placeholder={'/imgs/img-placeholder.svg'} {...other}/>
+      <img onLoad={() => setIsLoaded(true)} className={`${isLoaded ? 'visible' : 'invisible absolute'}`} src={src ? src : '/imgs/img-placeholder.svg'} alt={alt} placeholder={'/imgs/img-placeholder.svg'} {...other}/>
       {!isLoaded && <Skeleton className={`block w-full ${skeletonClassName}`} containerClassName={'flex'}/>}
     </div>
   )
--- src/components/content-viewer/ProjectBlock.tsx
+++ src/components/content-viewer/ProjectBlock.tsx
@@ -29,7 +29,7 @@ const ProjectBlock: FC<IProjectBlockProps> = ({data}) => {
   return (
     <div onClick={['large'].includes(viewport) ? onProjectClick : undefined} onMouseOver={() =>  setMouseOvered(true)} onMouseLeave={() => setMouseOvered(false)} className={'md:grid flex flex-col-reverse grid-cols-[minmax(100px,200px)_minmax(300px,1fr)] md:gap-8 md:space-y-0 space-y-reverse space-y-6 lg:p-8 rounded-lg lg:cursor-pointer lg:hover:shadow-md lg:hover:bg-secondary'}>
       <div>
-        <Image skeletonClassName={'h-[100px]'} className={`md:block md:w-auto md:h-auto w-[200px] inline-block items-stretch border-[3px] rounded-md ${mouseOvered ? 'border-primary-light' : 'border-primary-dark'}`} src={data.images && data.images.length !== 0 ? data.images[0] : '/imgs/img-placeholder.svg'} alt={data.name ? data.name : ''} loading={"lazy"} />
+        <Image skeletonClassName={'h-[100px]'} className={`md:block md:w-auto md:h-auto w-[200px] inline-block items-stretch border-[3px] rounded-md ${mouseOvered ? 'border-primary-light' : 'border-primary-dark'}`} src={data.images && data.images.length !== 0 ? data.images[0] : '/imgs/img-placeholder.svg'} alt={data.name ? data.name : ''} />
       </div>
       <div className={'grow'}>
         <Title onClick={!['large'].includes(viewport) ? onProjectClick : undefined} level={3} className={`mb-[10px] after:content-[""] after:inline-block after:h-[10px] after:w-[10px] after:bg-no-repeat after:bg-contain after:bg-center after:ml-2 cursor-pointer ${mouseOvered && ['large'].includes(viewport) ? 'after:bg-arrow-3-ac text-primary after:translate-x-[5px] after:translate-y-[-5px]' : 'after:bg-arrow-3 after:hover:bg-arrow-3-ac hover:text-primary after:hover:translate-x-[5px] after:hover:translate-y-[-5px]'}`}>{data.name}</Title>
