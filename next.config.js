43ba41f119550aeb75e1c37c0ca225d56b435825
Danil Efremov <danileph@bk.ru>
2023-07-10 10:22:22 +0300

    Some fixes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        // Apply this header to all routes in your application
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=60',
+          },
+        ],
+      },
+    ];
+  },
+  generateEtags: false,
   images: {
     remotePatterns: [
       {
